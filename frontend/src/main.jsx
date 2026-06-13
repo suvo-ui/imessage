c9bb3edefb2c6eea09a4b88f8fcecb27a52216aa
suvo-ui <rebantabiswas265@gmail,com>
2026-06-13 11:28:26 +0530

    Refactor App and main components for cleaner structure and improved readability

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import {ClerkProvider} from '@clerk/react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ClerkProvider } from "@clerk/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider afterSignOutUrl="/">
-<App />
-</ClerkProvider>
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
