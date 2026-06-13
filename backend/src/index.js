de7b9e2d5767ba9389906c8de91cb2994e0f7b0c
suvo-ui <rebantabiswas265@gmail,com>
2026-06-13 12:00:37 +0530

    Add Dockerfile and .dockerignore for multi-stage build setup; enhance server static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,14 +7,29 @@ import cors from "cors";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
+import fs from "fs";
+import path from "path";
+
 const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json()); //parse the request from the client
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
+
+//if the public directory eist serves the static files
+//this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 //just to ensure whether the server is up and running
 app.get("/health", (req, res) => {
