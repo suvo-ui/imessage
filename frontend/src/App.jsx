5fbe03a40b2d18ca7b202d2cb146b194cd583011
suvo-ui <rebantabiswas265@gmail,com>
2026-06-14 12:18:35 +0530

    feat: add axios instance, auth store with socket integration, and message formatting utility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,30 @@ import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import PageLoader from "./components/PageLoader.jsx";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import Toaster from "react-hot-toast";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return <PageLoader />;
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
@@ -33,6 +52,7 @@ function App() {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   );
